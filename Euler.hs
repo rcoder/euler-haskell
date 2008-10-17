97e070e257e27cbba989e74ceac1728017ea6cc8
Lennon Day-Reynolds <lennon@reed.edu>
2008-10-17 16:01:01 -0700

    added simple string split function to Euler lib

--- Euler.hs
+++ Euler.hs
@@ -1,4 +1,4 @@
-module Euler ( divides, factors, isPrime, pairs ) where
+module Euler ( divides, factors, isPrime, pairs, split ) where
 
 divides :: Integer -> Integer -> Bool
 divides i j = i `mod` j == 0
@@ -14,4 +14,10 @@ isPrime = null . factors
 pairs :: [a] -> [(a,a)]
 pairs []      = []
 pairs (x:xs)  = [(x,x)] ++ (map (\i -> (x,i)) xs) ++ pairs xs
+
+split :: String -> Char -> [String]
+split str delim = pre : (split post delim)
+  where pre     = fst chunks
+        post    = snd chunks
+        chunks  = break (== delim) str
 
